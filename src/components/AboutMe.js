0b39c51ae941abdc7825b1c05e2ad394739eb4f5
Nicholas Jaquez <nrjaquez@gmail.com>
2022-01-21 20:25:07 -0800

    home page image and info  uploaded, about me page updated

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,13 +5,13 @@ function AboutMe(props) {
   return (
     <div id="intro">
      <h1 id="welcome">
-         Welcome
+         About Me
      </h1>
      <img id="photo" src="assets/selfie.jpg" alt="pic-of-me" />
      <p>My name is Nicholas Jaquez and I'm a Web Developer.</p>
+     <p>I have experience in E-Commerce, SEO, and Content Management.</p>
      <p>I received my Certificate in Full-Stack Coding from UCLA in June of 2020.</p>
      <p>Feel free to look at my projects in the Portfolio tab and contact me if you have any questions.</p>
-     <p>Test</p>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "../components/Home.css";
 
 
 
 function Home(props) {
   return (
-    <div>
+    <div className="welcome-box">
      <h1>Welcome Everyone!</h1>
+     <p>My name is Nicholas and I am a Web Developer. Feel free to look through my portfolio and contact me for any questions.</p>
+     <img src="../assets/monitor-img.jpeg" alt="image_of_monitor" />
     </div>
   );
 }
